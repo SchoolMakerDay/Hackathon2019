8111388e6e59324eff36acaf0141dbc26abe0214
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 21:01:26 +0200

    Add files via upload

---
+++ test_touch/test_touch.ino
@@ -0,0 +1,16 @@
+//Wemos D1 R2
+//Test Touch
+//accende un led con il tocco
+//Corrispondenze GROVE>WEMOS D2>D0, D3>D1, D4>D2, D5>D3, D6>D4, D7>D5, D8>D6
+void setup() {
+  Serial.begin(9600);
+  pinMode(D0,OUTPUT);
+  pinMode(D3,INPUT);
+}
+
+void loop() {
+  int pValue=digitalRead(D3);
+  digitalWrite(D0,pValue);
+  Serial.println(pValue);
+  delay(100);
+}
