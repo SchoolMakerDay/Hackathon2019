58ae34fa541fdde22e382bf42ea3531e6a6f03c3
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:40:41 +0200

    Add files via upload

---
+++ test_bt/test_bt2.ino
@@ -0,0 +1,24 @@
+#include "SoftwareSerial.h"
+SoftwareSerial btSerial(D4, D3, false, 256);
+int rxChar;
+void setup() {
+  Serial.begin(9600);
+  btSerial.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  btSerial.print("$");  // Print three times individually
+  btSerial.print("$");
+  btSerial.print("$");  // Enter command mode
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+  btSerial.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+  btSerial.begin(9600);
+  delay(5000);
+  Serial.println("test bt su UNO"); 
+}
+
+void loop() {
+ if(btSerial.available()>0){
+    rxChar=btSerial.read();
+    Serial.write(rxChar);
+    delay(32);
+    btSerial.write(rxChar);
+  }
+}
