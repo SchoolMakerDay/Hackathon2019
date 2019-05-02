c829c03c3776a1bd2f4bab24657982f90f10bf5c
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:45:36 +0200

    Add files via upload

---
+++ test_ir_sensor/test_ir_sensor.ino
@@ -0,0 +1,15 @@
+void setup()  {
+    Serial.begin(9600);
+    pinMode(D1,INPUT_PULLUP);
+}
+void loop()  {
+    while(1)  {
+        delay(500);
+        if(digitalRead(D1)==LOW)  {
+            Serial.println("Somebody is here.");
+        }
+        else  {
+            Serial.println("Nobody.");
+        }
+    }
+}
