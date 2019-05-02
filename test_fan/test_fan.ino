85d2bce273a5c63b78e7788eee77b681697afbba
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:43:40 +0200

    Add files via upload

---
+++ test_fan/test_fan.ino
@@ -0,0 +1,16 @@
+void setup() {
+ pinMode(D2,OUTPUT);
+ pinMode(D3,INPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+  int cmd=digitalRead(D3);
+  if(cmd==HIGH){
+    digitalWrite(D2,HIGH);
+  }
+  else {
+    digitalWrite(D2,LOW);    
+  }
+
+}
