36ff6907bd78bb63434de6c48373cd070538ffc9
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:53:52 +0200

    Add files via upload

---
+++ test_pot/test_pot.ino
@@ -0,0 +1,17 @@
+//Wemos D1 R2
+//Test pot
+//ingresso analogico potenziometrico
+//Su WEMOS solo A0
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(D0,OUTPUT);
+}
+
+void loop() {
+  int pvalue=analogRead(A0);
+  int ledvalue=map(pvalue,0,1023,0,255);
+  analogWrite(D0,ledvalue);
+  Serial.println(pvalue);
+  delay(100);
+}
