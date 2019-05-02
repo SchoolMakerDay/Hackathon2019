ce3ad56a766060c3c52386049a30635e947222d2
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:41:41 +0200

    Add files via upload

---
+++ test_buzzer/test_buzzer.ino
@@ -0,0 +1,18 @@
+//Wemos D1 R2
+//Test Pushbutton
+//accende un led con la pressione
+//Corrispondenze GROVE>WEMOS D2>D0, D3>D1, D4>D2, D5>D3, D6>D4, D7>D5, D8>D6
+void setup() {
+  Serial.begin(9600);
+  pinMode(D0,OUTPUT);
+  pinMode(D3,INPUT);
+}
+
+void loop() {
+  int pValue=digitalRead(D3);
+  if(pValue==HIGH){
+    digitalWrite(D0,HIGH);
+    delay(500);
+    digitalWrite(D0,LOW);
+  }
+}
