467c8e8ddcc20ee0cc35bb377479540e53c7dec5
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:51:14 +0200

    Add files via upload

---
+++ test_light_sensor/test_light_sensor.ino
@@ -0,0 +1,14 @@
+//Wemos D1 R2
+//Test Light sensor
+//ingresso analogico di luminosità
+//Su WEMOS solo A0
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  int lvalue=analogRead(A0);
+  Serial.println(lvalue);
+  delay(1000);
+}
