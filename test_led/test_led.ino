1167b961cb2bf1e4f40b9b535e0ce1465216b7ba
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:48:01 +0200

    Add files via upload

---
+++ test_led/test_led.ino
@@ -0,0 +1,14 @@
+//Wemos D1 R2
+//Test LED
+//lampeggia freq 1 hz
+//Corrispondenze GROVE>WEMOS D2>D0, D3>D1, D4>D2, D5>D3, D6>D4, D7>D5, D8>D6
+void setup() {
+  pinMode(D0,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D0,HIGH);
+  delay(500);
+  digitalWrite(D0,LOW);
+  delay(500);
+}
