b005ac18ec21a4f7cf6c27ff4d653beec325dd27
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2019-05-02 20:58:33 +0200

    Add files via upload

---
+++ test_sound_sensor/test_sound_sensor.ino
@@ -0,0 +1,20 @@
+//Wemos D1 R2
+//Test sound sensor
+//ingresso analogico di luminosità
+//Su WEMOS solo A0
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(D0,OUTPUT);
+}
+
+void loop() {
+  int lvalue=analogRead(A0);
+  Serial.println(lvalue);
+  delay(100);
+  if(lvalue>100) {
+    digitalWrite(D0,HIGH);
+    delay(1000);
+    digitalWrite(D0,LOW);
+  }
+}
